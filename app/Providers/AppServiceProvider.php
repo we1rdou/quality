e9154fd5bcfe2a2702cadd44a17cc3a55cd24dc1
we1rdou <leoust15@gmail.com>
2026-02-03 22:43:55 -0500

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
         // Registrar middleware para admin y owner
         $router = app('router');
         $router->aliasMiddleware('admin.seller', function ($request, $next) {
